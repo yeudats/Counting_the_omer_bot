8b6c979ba793f75235dc1a44f62177ebc0ee0358
yeudats <yehudatseva@gmail.com>
2025-04-20 03:22:37 +0300

    add about page

--- send_telegram.py
+++ send_telegram.py
@@ -2,17 +2,21 @@ import requests
 import os
 import time
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
-
+# BOT_TOKEN = os.environ["BOT_TOKEN"]
+# CHAT_ID = os.environ["CHAT_ID"]
+BOT_TOKEN = '7871293487:AAEI-73NZUgtLD264QoKJ-Vya4kbCWZnLOM'
+CHAT_ID = 395009331
+counter = 7
+words = ["אחד", "שני", "שלושה", "ארבעה", "חמישה", "שישה"]
 def send_telegram_message():
-    counter = 7
+    global counter
+    global words
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": f"לא לשכוח ספירת העומר \n היום {counter} ימים לעומר שהם {counter/7} שבועות ו{counter%7} ימים"}
+    payload = {"chat_id": CHAT_ID, "text": f"לא לשכוח ספירת העומר\nהיום {counter} ימים לעומר שהם {words[counter//7]} שבועות ו{words[counter%7]} ימים"}
     counter += 1
     requests.post(url, data=payload)
 
 if __name__ == "__main__":
     while True:
-        if time.localtime().tm_hour == 2 and (time.localtime().tm_min == 55 or time.localtime().tm_min == 56):
+        if time.localtime().tm_hour == 3 and (time.localtime().tm_min == 19 or time.localtime().tm_min == 20) and time.localtime().tm_sec == 0:
             send_telegram_message()
